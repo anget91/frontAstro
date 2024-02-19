a1d34644596e0ebdde74de2117479b892ba1ce40
Yeider <yeiderjp12@gmail.com>
2024-02-19 15:00:06 -0500

    bug in the sidebar fixed

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,23 +1,9 @@
-import { useStore } from "@nanostores/react"
-import { ruta as rutaActual } from "../nanostore/atoms"
-
-
-export function NavButton ({children, ruta, url = "", text = "hola mundo"}) {
-  const $ruta = useStore(rutaActual)
-
-  const chooseRuta = () => {
-  
-    rutaActual.set(ruta)
-    console.log($ruta);
- 
-  } 
+export function NavButton({ children, ruta, url = "", text = "hola mundo" }) {
 
   return (
-    <a href={url} onClick={chooseRuta} className={`flex gap-5 w-[220px] items-center ${$ruta === ruta ? 'bg-vgray text-vgreen' : 'hover:bg-vgray'} mt-8 px-10 py-2 transition-all duration-300 rounded-full cursor-pointer`}>
+    <a href={url} className={`flex gap-5 w-[220px] items-center ${ruta == url ? 'bg-vgray text-vgreen' : 'hover:bg-vgray'} mb-8 px-10 py-2 transition-all duration-300 rounded-full cursor-pointer`}>
       {children}
       <h1 className="text-md font-semibold text-[#393939]"> {text} </h1>
     </a>
-    
   )
- 
 }
